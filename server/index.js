58d82880ce0d9c34efa4bb16a59a9b931ef11515
taduyhieu <taduyhieucntt98@gmail.com>
2022-04-07 17:39:13 +0700

    add socket for rank

--- server/index.js
+++ server/index.js
@@ -1,30 +1,61 @@
-var express = require('express')
+var express = require("express");
 const http = require("http");
 var app = express();
 const server = http.createServer(app);
 
 const socketIo = require("socket.io")(server, {
-    cors: {
-        origin: "*",
-    }
-  });
+  cors: {
+    origin: "*",
+  },
+});
 
+const users = [
+  {
+    id: "123",
+    name: "Hieu",
+  },
+];
 
+const getUser = (id) => users[id];
+let listUser = [];
 socketIo.on("connection", (socket) => {
+  console.log(socket.handshake.auth);
+  const auth = socket.handshake.auth;
   console.log("New client connected" + socket.id);
 
   socket.emit("getId", socket.id);
 
-  socket.on("sendDataClient", function(data) {
-    console.log(data)
-    socketIo.emit("sendDataServer", { data });
-  })
+  socket.on("create", function (room) {
+    console.log(socket.id + " joined to " + room);
+    // console.log(room);
+    listUser.push({
+      socketId: socket.id,
+      name: auth.name,
+      roomId: room,
+      point: 0,
+    });
+    socket.join(room);
+    socketIo.to(room).emit("listUser", { listUser });
+  });
+
+  socket.on("sendDataClient", function (data) {
+    console.log(data.roomId);
+    socketIo.to(data.roomId).emit("sendDataServer", { data });
+  });
 
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
+    console.log("Client disconnected: " + socket.id);
+    let disconnectedUser = listUser.filter(
+      (value) => value.socketId == socket.id
+    )[0];
+    listUser = listUser.filter((value) => value.socketId != socket.id);
+    console.log(listUser);
+    if (disconnectedUser) {
+      socketIo.to(disconnectedUser.roomId).emit("listUser", { listUser });
+    }
   });
 });
 
 server.listen(3000, () => {
-    console.log('Server đang chay tren cong 3000');
+  console.log("Server đang chay tren cong 3000");
 });
--- web/src/App.js
+++ web/src/App.js
@@ -1,86 +1,127 @@
 import React, { useState, useEffect, useRef } from "react";
 import socketIOClient from "socket.io-client";
-import './App.css'
+import "./App.css";
 const host = "http://localhost:3000";
 
 function App() {
   const [mess, setMess] = useState([]);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [id, setId] = useState();
+  const [roomId, setRoomId] = useState();
+  const [name, setName] = useState();
 
   const socketRef = useRef();
   const messagesEnd = useRef();
 
-  useEffect(() => {
-    socketRef.current = socketIOClient.connect(host)
-  
-    socketRef.current.on('getId', data => {
-      setId(data)
-    })
+  var query = window.location.search.substring(1);
+  console.log(query);
 
-    socketRef.current.on('sendDataServer', dataGot => {
-      setMess(oldMsgs => [...oldMsgs, dataGot.data])
-      scrollToBottom()
-    })
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    console.log(query); //"app=article&act=news_content&aid=160990"
+    var vars = query.split("&");
+    console.log(vars); //[ 'app=article', 'act=news_content', 'aid=160990' ]
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      console.log(pair); //[ 'app', 'article' ][ 'act', 'news_content' ][ 'aid', '160990' ]
+      if (pair[0] == variable) {
+        return pair[1];
+      }
+    }
+    return false;
+  }
+
+  useEffect(() => {
+    const roomId = getQueryVariable("roomid");
+    const name = getQueryVariable("name");
+    setRoomId(roomId);
+    setName(name);
+    console.log(roomId);
+
+    socketRef.current = socketIOClient.connect(host, {
+      auth: {
+        name: name,
+      },
+    });
+
+    socketRef.current.on("getId", (data) => {
+      setId(data);
+    });
+
+    socketRef.current.on("setUser", (data) => {
+      console.log(data);
+    });
+
+    socketRef.current.on("listUser", (data) => {
+      console.log(data);
+    });
+
+    socketRef.current.emit("create", `room${roomId}`);
+
+    socketRef.current.on("sendDataServer", (dataGot) => {
+      setMess((oldMsgs) => [...oldMsgs, dataGot.data]);
+      scrollToBottom();
+    });
 
     return () => {
-      socketRef.current.disconnect();
+      socketRef.current.disconnect((id) => {
+        console.log("disconnect");
+      });
     };
   }, []);
 
   const sendMessage = () => {
-    if(message !== null) {
+    if (message !== null) {
       const msg = {
-        content: message, 
-        id: id
-      }
-      socketRef.current.emit('sendDataClient', msg)
-      setMessage('')
+        content: message,
+        id: id,
+        roomId: `room${roomId}`,
+      };
+      socketRef.current.emit("sendDataClient", msg);
+      setMessage("");
     }
-  }
+  };
 
   const scrollToBottom = () => {
     messagesEnd.current.scrollIntoView({ behavior: "smooth" });
-  }
-  
+  };
 
-  const renderMess =  mess.map((m, index) => 
-        <div key={index} className={`${m.id === id ? 'your-message' : 'other-people'} chat-item`}>
-          {m.content}
-        </div>
-      )
+  console.log(mess);
+  const renderMess = mess.map((m, index) => (
+    <div
+      key={index}
+      className={`${m.id === id ? "your-message" : "other-people"} chat-item`}
+    >
+      {m.content}
+    </div>
+  ));
 
   const handleChange = (e) => {
-    setMessage(e.target.value)
-  }
+    setMessage(e.target.value);
+  };
 
   const onEnterPress = (e) => {
-    if(e.keyCode == 13 && e.shiftKey == false) {
-      sendMessage()
+    if (e.keyCode == 13 && e.shiftKey == false) {
+      sendMessage();
     }
-  }
+  };
 
   return (
     <div class="box-chat">
       <div class="box-chat_message">
-      {renderMess}
-      <div style={{ float:"left", clear: "both" }}
-             ref={messagesEnd}>
-        </div>
+        {renderMess}
+        <div style={{ float: "left", clear: "both" }} ref={messagesEnd}></div>
       </div>
 
       <div class="send-box">
-          <textarea 
-            value={message}  
-            onKeyDown={onEnterPress}
-            onChange={handleChange} 
-            placeholder="Nhập tin nhắn ..." 
-          />
-          <button onClick={sendMessage}>
-            Send
-          </button>
+        <textarea
+          value={message}
+          onKeyDown={onEnterPress}
+          onChange={handleChange}
+          placeholder="Nhập tin nhắn ..."
+        />
+        <button onClick={sendMessage}>Send</button>
       </div>
-
     </div>
   );
 }
